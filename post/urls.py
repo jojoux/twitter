eedc6a1b12d1052a57ee69d13b39ecac05bd4d57
fabianacsd <44444b1111111@gmail.com>
2024-08-17 11:45:43 -0400

    create

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView, DetailPageView
+from .views import HomePageView, DetailPageView, CreatePageView
 
 urlpatterns=[
     path("", HomePageView.as_view(), name="home"), 
     
-    path("detail/<int:pk>", DetailPageView.as_view(), name="detail")
+    path("detail/<int:pk>", DetailPageView.as_view(), name="detail"),
+    
+    path("post/create", CreatePageView.as_view(), name="create"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 # Create your views here.
 
@@ -15,4 +16,11 @@ class DetailPageView (DetailView):
     model= Post
     template_name= "detail.html"
 
+
+class CreatePageView(CreateView):
+
+    model= Post
+    template_name= "create.html"
+
+    fields=["titulo", "descripcion", "autor"]
     
